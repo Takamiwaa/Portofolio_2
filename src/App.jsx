58b82cb1842b2115d37506f3f76af8ce5d2a5923
Hizkia Siahaan <siahaanhizkia548@gmail.com>
2025-06-25 01:59:59 +0700

    none

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Hero';
 import About from './components/About';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 function App() {
 
   return (
@@ -10,6 +11,7 @@ function App() {
       <Home />
       <About />
       <Footer />
+      <ScrollToTop />
     </>
   )
 }
