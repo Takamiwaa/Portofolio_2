6ebc1cdb0897353fe23d5ab2faa9bc04c1ded2ee
Hizkia Siahaan <siahaanhizkia548@gmail.com>
2025-06-25 00:23:55 +0700

    none

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
+import Footer from './components/Footer';
 function App() {
 
   return (
@@ -14,6 +15,7 @@ function App() {
       <Projects />
       <Experience />
       <Skills />
+      <Footer />
     </>
   )
 }
