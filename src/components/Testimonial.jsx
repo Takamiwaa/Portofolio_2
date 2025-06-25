725f643c68adf6ce6a4007ec1523fa86ed286b83
Hizkia Siahaan <siahaanhizkia548@gmail.com>
2025-06-25 22:46:49 +0700

    none

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -3,8 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 import Swal from "sweetalert2";
 
 // Inisialisasi Supabase client
-const supabaseUrl = "https://ssmqxtbgghewocbmghqk.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzbXF4dGJnZ2hld29jYm1naHFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4Mjc3MjEsImV4cCI6MjA2NjQwMzcyMX0.WrlO0cuSIjw1R5jZqUR4K5OG05iAIi03Eai6E9OT6mQ";
+const supabaseUrl = "#";
+const supabaseKey = "#";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const CustomModal = ({ isOpen, onClose, children }) => {
