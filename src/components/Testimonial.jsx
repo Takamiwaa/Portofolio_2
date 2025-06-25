56fee6e3e3fec7baf7d6c1c2e9bad5d06fccc197
Hizkia Siahaan <siahaanhizkia548@gmail.com>
2025-06-25 15:23:57 +0700

    none

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -14,7 +14,7 @@ const CustomModal = ({ isOpen, onClose, children }) => {
         <div className="fixed inset-0 z-50 flex items-center justify-center">
             {/* Backdrop */}
             <div
-                className="absolute  transition-opacity"
+                className="absolute inset-0 backdrop-blur-md bg-black/50 transition-opacity"
                 onClick={onClose}
             />
 
